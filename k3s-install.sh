c1b180842251c37975fd4f8fe8b9aac128676978
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-12 14:19:51 +0300

    Update k3s-install.sh

--- k3s-install.sh
+++ k3s-install.sh
@@ -9,23 +9,3 @@ export KUBECONFIG=~/.kube/config
 echo "export KUBECONFIG=~/.kube/config" >> ~/.bashrc
 source ~/.bashrc
 
-# Kullanıcının ev dizinini al
-USER_HOME=$(eval echo ~$USER)
-
-# .kube dizinini oluştur
-mkdir -p $USER_HOME/.kube
-
-# k3s kubeconfig dosyasını kopyala ve yeniden adlandır
-cp /etc/rancher/k3s/k3s.yaml $USER_HOME/.kube/config
-
-# Dosya sahipliğini güncelle
-chown -R $USER:$USER $USER_HOME/.kube
-
-# Diğer kullanıcıların dosyayı okumasını engelle
-chmod -R go-r $USER_HOME/.kube/config
-
-# KUBECONFIG çevre değişkenini ayarla
-echo "export KUBECONFIG=$USER_HOME/.kube/config" >> $USER_HOME/.bashrc
-
-echo "$USER kullanıcısı için k3s yapılandırması tamamlandı."
-
