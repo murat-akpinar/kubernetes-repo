f21001b04faf25bf0ff64df9c0196b09a60daf0a
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-14 08:11:39 +0300

    Update argocd.sh

--- ArgoCD/argocd.sh
+++ ArgoCD/argocd.sh
@@ -4,6 +4,7 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-echp ""
+echo ""
+echo ""
 echo "ArgoCD Web UI Parolası"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
